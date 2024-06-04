12bee4bc5b45f6619a1267cca11e223b5cab7a55
parovoz33 <parovoz33@MAIL.RU>
2024-06-04 15:17:47 +0300

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -1,13 +1,14 @@
 random_number = int(input('Ведите число от 3 до 20 :'))
 
-result = []
+result = ""
 for i in range(1, random_number):
     if random_number < 3 or random_number > 20:
         print('Неверное число')
         break
-    for j in range(1, random_number // 2 + 1):
+    for j in range(1, random_number):
 
-        if random_number % (i + j) == 0 and i != j and i != j // 2:
-            result.append(i)
-            result.append(j)
+        if random_number % (i + j) == 0 and i < j:
+            result += str(i)
+            result += str(j)
+
 print(result)
