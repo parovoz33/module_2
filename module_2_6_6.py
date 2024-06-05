0a0bd75fea7ce3c7f1e058aa662a693723744349
parovoz33 <parovoz33@MAIL.RU>
2024-06-05 14:16:30 +0300

    Add files via upload

---
+++ module_2_6_6.py
@@ -0,0 +1,14 @@
+def print_params(a=1, b='Строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+
+values_list = [5, 'man', None]
+
+values_dict = {'a': 1, 'b': 'example', 'c': True}
+
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
