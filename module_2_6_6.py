68532a500dcd16686a2146fe1c1619f7089368b8
parovoz33 <parovoz33@MAIL.RU>
2024-06-05 14:45:45 +0300

    Update module_2_6_6.py

--- module_2_6_6.py
+++ module_2_6_6.py
@@ -3,7 +3,8 @@ def print_params(a=1, b='Строка', c=True):
 
 
 print_params()
-
+print_params(b = 25)
+print_params(c = [1,2,3])
 values_list = [5, 'man', None]
 
 values_dict = {'a': 1, 'b': 'example', 'c': True}
